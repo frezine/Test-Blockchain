62647a9f2cdd2e9bbdd88e8887cd0bfffe3fb605
frezine <frpv4@hotmail.com>
2018-02-18 13:02:08 -0800

    Add comments to TestBlock

--- TestBlock.py
+++ TestBlock.py
@@ -2,6 +2,7 @@ from sys import getsizeof
 from Block import Block
 
 
+##Initial Blockchain
 block1 = Block("Fred sends 5 to Holly")
 print("Block 1 Trans: " + block1.getTransactions())
 print("Block 1 Hash: " + block1.getCurrentHash())
@@ -14,13 +15,15 @@ print()
 
 block3 = Block("Rachel sends 15 to Brian",block2)
 print("Block 3 Trans: " + block3.getTransactions())
-print("Block 2 Hash: " + block3.getCurrentHash())
+print("Block 3 Hash: " + block3.getCurrentHash())
 print()
 
 
+##Test Blockchain after changing Block 1
 print()
 print("Changing Block 1 Transactions")
 block1.setTransactions("Samy sends 200 to Holly")
+
 print("Block 1 Trans: " + block1.getTransactions())
 print("Block 1 Hash: " + block1.getCurrentHash())
 print()
@@ -30,5 +33,23 @@ print("Block 2 Hash: " + block2.getCurrentHash())
 print()
 
 print("Block 3 Trans: " + block3.getTransactions())
-print("Block 2 Hash: " + block3.getCurrentHash())
+print("Block 3 Hash: " + block3.getCurrentHash())
+print()
+
+
+##Test Blockchain after changing Block 2
+print()
+print("Changing Block 2 Transactions")
+block2.setTransactions("Holly sends 1000 to Samy")
+
+print("Block 1 Trans: " + block1.getTransactions())
+print("Block 1 Hash: " + block1.getCurrentHash())
+print()
+
+print("Block 2 Trans: " + block2.getTransactions())
+print("Block 2 Hash: " + block2.getCurrentHash())
+print()
+
+print("Block 3 Trans: " + block3.getTransactions())
+print("Block 3 Hash: " + block3.getCurrentHash())
 print()
