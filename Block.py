bb0ff8ded01a788adbe58e9529952aab31844647
frezine <frpv4@hotmail.com>
2018-02-18 14:12:14 -0800

    Extra Functions

--- Block.py
+++ Block.py
@@ -19,14 +19,28 @@ class Block():
         """Set Transaction String"""
         self.transactions = transactions
 
+    def getRawString(self):
+        """Return 'hash(previousBlock)':'transactions'"""
+        if self.prevBlock == None:
+            return "{0}:{1}".format("Genesis",self.transactions).encode('utf-8')
+        else:
+            return "{0}:{1}".format(self.prevBlock.getCurrentHash(),self.transactions).encode('utf-8')
+
     
     def getCurrentHash(self):
         """Return sha256 hash of 'hash(previousBlock)':'transactions'"""
         if self.prevBlock == None:
-            return sha256("{0}:{1}".format("Genesis",self.transactions).encode('utf-8')).hexdigest()
+            return sha256(self.getRawString()).hexdigest()
         else:
-            return sha256("{0}:{1}".format(self.prevBlock.getCurrentHash(),self.transactions).encode('utf-8')).hexdigest()
+            return sha256("{0}:{1}".format(self.getRawString()).hexdigest()
 
+
+    def getPreviousRawString(self):
+        """Get Previous Raw String"""
+        if self.prevBlock == None:
+            return None
+        else:
+            return self.prevBlock.getRawString()
 
     def getPreviousHash(self):
         """Get Previous Hash"""
